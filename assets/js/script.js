4273332d0ce6bf467bd8aac19722cd11fd7cf174
glrmrissi <guilhermerissi04112006@gmail.com>
2024-05-10 17:35:45 -0300

    btnTop add

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,22 @@ closeBtn.addEventListener('click', function () {
     headerMobal.classList.remove('active');
     console.log('fechou');
 });
+
+
+let btn = document.getElementById('btnTop')
+
+btn.addEventListener("click", function(){
+    window.scrollTo(0, 0)
+})
+
+function ocultar() {
+  if (window.scrollY < 10) {
+      btn.style.visibility = "hidden";
+      btn.style.opacity = "0";
+  } else {
+      btn.style.visibility = "visible";
+      btn.style.opacity = "1";
+  }
+}
+
+window.addEventListener("scroll", ocultar)
